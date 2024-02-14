c5a0177823eeb4d664d2c9d30cf15427c1087284
ehmedosmanov <ehmedosmanov@gmail.com>
2024-02-14 16:41:59 +0400

    feat: implemented get video duration from s3 bucket

--- server/src/helpers/s3.js
+++ server/src/helpers/s3.js
@@ -1,20 +1,19 @@
 import {
   S3Client,
-  ListBucketsCommand,
   PutObjectCommand,
   DeleteObjectCommand,
   GetObjectCommand
 } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import dotenv from 'dotenv'
+import { getVideoDurationInSeconds } from 'get-video-duration'
 dotenv.config()
 
 const bucketName = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_BUCKET_ACCESS_KEY
 const secretAccessKey = process.env.AWS_BUKCET_SECRET_KEY
-
 const s3Client = new S3Client({
   region,
   credentials: {
@@ -52,4 +51,19 @@ export async function getObjectSignedUrl(key) {
   const seconds = 60
   const url = await getSignedUrl(s3Client, command, { expiresIn: seconds })
   return url
+}
+
+export const getVideoDurationFromS3 = async key => {
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: key
+    }
+    const command = new GetObjectCommand(params)
+    const s3Object = await s3Client.send(command)
+    const duration = await getVideoDurationInSeconds(s3Object.Body)
+    return duration
+  } catch (error) {
+    throw error
+  }
 }
